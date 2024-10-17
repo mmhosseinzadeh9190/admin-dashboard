eca9ccbd72c37fa615c812f987b3c11600d9b5f6
Mohammad Mahdi Hosseinzadeh <mohammadmahdihosseinzadeh68@gmail.com>
2024-10-17 22:06:09 +0330

    refactor: refactor ProjectCard & ProgressBar

--- src/features/projects/ProjectCard.tsx
+++ src/features/projects/ProjectCard.tsx
@@ -1,29 +1,64 @@
-import { Clock, Paperclip2, TickSquare } from "iconsax-react";
+import { ReactNode, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Clock,
+  Paperclip2,
+  TickSquare,
+  Edit,
+  Eye,
+  Trash,
+  TimerStart,
+} from "iconsax-react";
 import { iconColor } from "../../styles/GlobalStyles";
-import Spinner from "../../ui/Spinner";
-import { daysUntil } from "../../utils/helpers";
+import {
+  addDefaultSrc,
+  capitalizeAllFirstLetters,
+  capitalizeFirstLetter,
+  daysUntil,
+} from "../../utils/helpers";
 import ProgressBar from "../../ui/ProgressBar";
-import Button from "../../ui/Button";
 import { Project } from "../../services/apiProjects";
 import { useTeams } from "../dashboard/useTeams";
 import { useUsers } from "../dashboard/useUsers";
+import { useUser } from "../authentication/useUser";
+import Spinner from "../../ui/Spinner";
+import CustomDropdown from "../../ui/CustomDropdown";
+import Modal from "../../ui/Modal";
 
 interface ProjectCardProps {
   project: Project;
 }
 
 function ProjectCard({ project }: ProjectCardProps) {
-  const { teams, isLoading: teamsIsLoading, error: teamsError } = useTeams();
+  const navigate = useNavigate();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState<ReactNode>(null);
+
+  const { user } = useUser();
+  const { teams, isLoading: teamsIsLoading, error: teamsError } = useTeams();
   const { users, isLoading: usersIsLoading, error: usersError } = useUsers();
 
   if (teamsIsLoading || usersIsLoading) return <Spinner />;
+
+  const handleOpenModal = (content: ReactNode) => {
+    setModalContent(content);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setModalContent(null);
+  };
 
   const teamMembers = teams?.data?.find(
     (team) => team.id === project.team,
   )?.members;
 
+  const placeholderAvatar = "/public/avatarPlaceholder.png";
+
   const daysLeft = daysUntil(project.deadline!);
+
   const deadlineText = daysLeft < 0 ? "days past" : "days left";
 
   let textColor = "text-gray-700",
@@ -42,19 +77,71 @@ function ProjectCard({ project }: ProjectCardProps) {
     iconClockColor = "#ed1515";
   }
 
+  const dropdownItems = [
+    {
+      icon: <Eye size="16" variant="Linear" />,
+      label: "See details",
+      onClick: () => navigate(`/projects/${project.id}`),
+    },
+  ];
+
+  if (user?.id === project.created_by) {
+    dropdownItems.push(
+      {
+        icon: <Edit size="16" variant="Linear" />,
+        label: "Edit",
+        onClick: () => navigate(`/projects/${project.id}`),
+      },
+      {
+        icon: <Trash size="16" variant="Linear" />,
+        label: "Delete",
+        onClick: () =>
+          handleOpenModal(<p>Are you sure you want to delete this project?</p>),
+      },
+    );
+  }
+
+  if (user?.id === project.created_by && project.status === "pending") {
+    dropdownItems.push({
+      icon: <TimerStart size="16" variant="Linear" />,
+      label: "Start",
+      onClick: () =>
+        handleOpenModal(<p>Are you sure you want to start this project?</p>),
+    });
+  }
+
+  if (user?.id === project.created_by && project.status === "run") {
+    dropdownItems.push({
+      icon: <TickSquare size="16" variant="Linear" />,
+      label: "Done",
+      onClick: () =>
+        handleOpenModal(
+          <p>Are you sure you want to mark this project as done?</p>,
+        ),
+    });
+  }
+
   return (
-    <div className="flex flex-col gap-1.5 rounded-[20px] bg-white p-4">
+    <div className="flex flex-col gap-1.5 rounded-2.5xl bg-white p-4 shadow-sm">
       <div className="flex items-center justify-between gap-4">
         <h3
           className={`truncate text-sm font-semibold tracking-0.1 text-gray-900 ${
             project.status === "done" ? "line-through" : ""
           }`}
         >
-          {project.name}
+          {capitalizeAllFirstLetters(project.name!)}
         </h3>
-        <Button className="select-none">
-          <img src="/src/assets/ellipsis.svg" alt="Options" />
-        </Button>
+
+        <CustomDropdown
+          items={dropdownItems.map((item) => ({
+            ...item,
+            onClick: item.onClick,
+          }))}
+        />
+
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          {modalContent}
+        </Modal>
       </div>
 
       <>
@@ -65,7 +152,7 @@ function ProjectCard({ project }: ProjectCardProps) {
                 key={team.id}
                 className="font-roboto text-sm tracking-0.1 text-gray-700"
               >
-                {team.name}
+                {capitalizeFirstLetter(team.name!)}
               </div>
             ),
         )}
@@ -98,25 +185,30 @@ function ProjectCard({ project }: ProjectCardProps) {
         <ProgressBar
           totalTasks={project.tasks?.length ?? 0}
           completedTasks={project.tasks_done?.length ?? 0}
+          progressPercentagePosition="absolute bottom-1.5 right-0"
+          height="h-1"
         />
       </div>
 
       <div className="mt-2 flex items-center gap-2.5">
-        {users?.data?.map(
-          (user) =>
-            teamMembers?.slice(0, 5).includes(String(user.id)) && (
-              <img
-                key={user.id}
-                src={user.avatar_url!}
-                alt={user.name!}
-                className="h-8 w-8 rounded-full object-cover object-center"
-              />
-            ),
-        )}
-        {teamMembers && teamMembers.length > 5 && (
+        {teamMembers?.slice(0, 4).map((memberId) => {
+          const user = users?.data?.find(
+            (user) => String(user.id) === memberId,
+          );
+          return (
+            <img
+              key={user?.id}
+              src={user?.avatar_url || placeholderAvatar}
+              alt={user?.name!}
+              onError={(e) => addDefaultSrc(e, "avatar")}
+              className="h-8 w-8 rounded-full object-cover object-center"
+            />
+          );
+        })}
+        {teamMembers && teamMembers.length > 4 && (
           <div className="flex h-8 w-8 items-center justify-center rounded-full border border-gray-300">
-            <span className="mr-px mt-px text-xs text-gray-700">
-              +{teamMembers.length - 5}
+            <span className="mr-px mt-px select-none text-xs text-gray-700">
+              +{teamMembers.length - 4}
             </span>
           </div>
         )}
--- src/ui/ProgressBar.tsx
+++ src/ui/ProgressBar.tsx
@@ -1,19 +1,32 @@
 interface ProgressBarProps {
   totalTasks: number;
   completedTasks: number;
+  height: string;
+  className?: string;
+  progressPercentagePosition?: string;
 }
 
-function ProgressBar({ totalTasks, completedTasks }: ProgressBarProps) {
+function ProgressBar({
+  totalTasks,
+  completedTasks,
+  height,
+  className,
+  progressPercentagePosition,
+}: ProgressBarProps) {
   const progressPercentage =
     totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0;
 
   return (
-    <div className="relative h-1 w-full rounded-full bg-gray-300">
+    <div
+      className={`${height} ${className ? className : ""} relative w-full rounded-full bg-gray-300`}
+    >
       <div
         className="h-full rounded-full bg-green-600"
         style={{ width: `${progressPercentage}%` }}
       />
-      <span className="absolute bottom-1.5 right-0 font-roboto text-sm text-gray-700">
+      <span
+        className={`${progressPercentagePosition ? progressPercentagePosition : ""} font-roboto text-sm text-gray-700`}
+      >
         {Math.round(progressPercentage)}%
       </span>
     </div>

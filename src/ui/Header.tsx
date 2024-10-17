e4908608474bff3211fd94216cde4f6bea99ce4c
Mohammad Mahdi Hosseinzadeh <mohammadmahdihosseinzadeh68@gmail.com>
2024-10-17 19:55:46 +0330

    feat: add SearchBar component & update Header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,20 +1,17 @@
-import Button from "./Button";
+import DarkModeToggle from "./DarkModeToggle";
+import SearchBar from "./SearchBar";
 import User from "./User";
 
 function Header() {
+  const handleSearch = (searchTerm: string) => {
+    console.log("Searched for:", searchTerm);
+  };
+
   return (
     <header className="bored flex items-center justify-end gap-4 border-b border-gray-200 px-12 py-3">
-      <Button className="flex items-center gap-2 rounded-[10px] bg-gray-200 px-3.5 py-2 text-sm font-semibold tracking-0.1 text-gray-700 hover:bg-gray-300 disabled:cursor-not-allowed">
-        <img src="/src/assets/search.svg" alt="" role="button" />
-        <span>Find</span>
-      </Button>
-
-      <Button className="flex items-center gap-2 rounded-[10px] bg-primary-800 px-3.5 py-2 text-sm font-semibold tracking-0.1 text-white hover:bg-primary-900 disabled:cursor-not-allowed">
-        <img src="/src/assets/plus.svg" alt="" role="button" />
-        <span>New</span>
-      </Button>
-
+      <SearchBar onSearch={handleSearch} />
       <User />
+      <DarkModeToggle />
     </header>
   );
 }
---
+++ src/ui/SearchBar.tsx
@@ -0,0 +1,48 @@
+import { SearchNormal1 } from "iconsax-react";
+import React, { useState } from "react";
+import { iconColor } from "../styles/GlobalStyles";
+import Button from "./Button";
+
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    onSearch(value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(searchTerm);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mr-auto max-w-md">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <div className="relative">
+        <Button type="submit" className="absolute left-3 top-3">
+          <SearchNormal1 size="16" color={iconColor} variant="Linear" />
+        </Button>
+
+        <input
+          type="text"
+          id="search"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Search..."
+          className="block w-full rounded-full border border-gray-200 bg-gray-100 py-2.5 pl-9 pr-4 font-roboto text-sm tracking-0.1 text-gray-800 placeholder:text-gray-500 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200"
+          required
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchBar;
